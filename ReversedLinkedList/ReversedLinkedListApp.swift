65dbc7d93998ef2446f36781d3a7cabd69e2f230
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-09-24 14:20:41 +0300

    Initial Commit

---
+++ ReversedLinkedList/ReversedLinkedListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReversedLinkedListApp.swift
+//  ReversedLinkedList
+//
+//  Created by ramil on 24.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct ReversedLinkedListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
